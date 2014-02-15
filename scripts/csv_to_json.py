c06f3cc4dec3c2342b56cc0ab71bb0c273fe6394
Dhanannjay Deo <dhanannjay.deo@kitware.com>
2014-02-15 16:53:35 -0500

    Creating geojson

--- scripts/csv_to_json.py
+++ scripts/csv_to_json.py
@@ -7,7 +7,7 @@ and write back in geo-json format which will be easy to read by webapp
 {"type":"Feature","id":"01","properties":{"name":"Alabama","density":94.65},"geometry":{"type":"Polygon","coordinates":[[[-87.359296,35.00118], .. ]]}},
 
 """
-
+import json
 import csv
 import sys
 import os
@@ -20,21 +20,39 @@ geojson = {"type":"FeatureCollection",
            "features": []}
 
 
-current_feature = {"name": ""}
+def new_feature():
+    return {"type" : "Feature", "id" : "" , "properties" : { "name": "", "state" : ""},
+                   "geometry" : {"type" : "polygon", "coordinates" : []}}
+
+current_feature = new_feature()
 
 for r in reader:
     # Skip first row of titles
     count = count + 1
     if count == 0:
-        print "Skip"
+        # print "Skip"
         continue
 
     # Find out if we are dealing with the same constituency
-    if r[7] != current_feature["name"]:
+    if r[7] != current_feature["properties"]["name"]:
         # Not so create a new feature and insert current feature
-        geojson["features"].append(current_feature)
-        current_feature = {"name": r[7]}
-        print "New constituency: ", r[7]
+        if current_feature["properties"]["name"] != "":
+            geojson["features"].append(current_feature)
+            print "Feature Saved: ", current_feature["properties"]["name"], current_feature["properties"]["state"],  ",  ", len(current_feature["geometry"]["coordinates"]), " Coordinates."
+            # Create new featire
+        current_feature = new_feature()
+        current_feature["properties"]["name"]  = r[7]
+        current_feature["properties"]["state"]  = r[8]
+
+    # Aggregate the coordinates
+    current_feature["geometry"]["coordinates"].append([float(r[0]), float(r[1])])
 
 print "Total constituencies: ", len(geojson["features"])
+
+# output
+fout = open(rootpath + "/../web/static/constituencies.json","w")
+# fout.write(json.dumps(geojson,sort_keys=True, indent=4, separators=(',', ': ')))
+fout.write(json.dumps(geojson))
+fout.close()
+
 
